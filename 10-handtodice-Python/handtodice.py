b4ed4ffbd5e4542a569a5adc44374463b1122987
Tejasvy25 <vardisaitejasvy@msitprogram.net>
2021-07-13 10:07:59 +0530

    day-1-3

--- 10-handtodice-Python/handtodice.py
+++ 10-handtodice-Python/handtodice.py
@@ -6,5 +6,12 @@
 # Hint: You might find // and % useful here, and also getKthDigit().
 
 def handtodice(hand):
-	# your code goes here
-	pass
+    count=0
+    l=[] 
+    while (hand>0): # 123> 0 , 12 >0
+        a=int(hand%10) #12.3 a = 3 , 12%10 = 1.2 a= 2
+        hand=int(hand/10) #123 = 123/10 = 12, 12/10 =1
+        l.insert(count,a) # 0th place a=3
+        count=count+1
+    l.reverse()
+    return tuple(l)
