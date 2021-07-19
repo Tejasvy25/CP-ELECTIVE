ec444f27b3dadcbb50f41392363fa36363f3bcff
Tejasvy25 <vardisaitejasvy@msitprogram.net>
2021-07-19 06:10:26 +0530

    commit

--- 01-stacks-Python/stacks.py
+++ 01-stacks-Python/stacks.py
@@ -1,3 +1,4 @@
+  
 """Add a couple methods to our LinkedList class,
 and use that to implement a Stack.
 You have 4 functions below to fill in:
@@ -7,12 +8,12 @@ why is it easier to add an "insert_first"
 function than just use "append"?"""
 
 class Element(object):
-    def _init_(self, value):
+    def __init__(self, value):
         self.value = value
         self.next = None
         
 class LinkedList(object):
-    def _init_(self, head=None):
+    def __init__(self, head=None):
         self.head = head
         
     def append(self, new_element):
@@ -25,14 +26,13 @@ class LinkedList(object):
             self.head = new_element
 
     def insert_first(self, new_element):
-        # "Insert new element as the head of the LinkedList"
-        # #pass
+        "Insert new element as the head of the LinkedList"
         new_element.next = self.head
         self.head = new_element
+        
 
     def delete_first(self):
-        # "Delete the first (head) element in the LinkedList as return it"
-        #pass
+        "Delete the first (head) element in the LinkedList as return it"
         if self.head:
             deleted_element = self.head
             temp = deleted_element.next
@@ -42,15 +42,15 @@ class LinkedList(object):
             return None
 
 class stack(object):
-    def _init_(self,top=None):
+    def __init__(self,top=None):
         self.ll = LinkedList(top)
 
     def push(self, new_element):
-        # "Push (add) a new element onto the top of the stack"
-        #pass
+        "Push (add) a new element onto the top of the stack"
         self.ll.insert_first(new_element)
+        
 
     def pop(self):
-        # "Pop (remove) the first element off the top of the stack and return it"
-        #pass
-        return self.ll.delete_first()
+        "Pop (remove) the first element off the top of the stack and return it"
+        self.ll.delete_first()
+    
--- 01-stacks-Python/test_solution.py
+++ 01-stacks-Python/test_solution.py
@@ -1,32 +1,54 @@
+"""Add a couple methods to our LinkedList class,
+and use that to implement a Stack.
+You have 4 functions below to fill in:
+insert_first, delete_first, push, and pop.
+Think about this while you're implementing:
+why is it easier to add an "insert_first"
+function than just use "append"?"""
 
-import os,sys
-sys.path.append(os.getcwd())
-from stacks import stack
-from stacks import Element  
-import pytest
+class Element(object):
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList(object):
+    def __init__(self, head=None):
+        self.head = head
+        
+    def append(self, new_element):
+        current = self.head
+        if self.head:
+            while current.next:
+                current = current.next
+            current.next = new_element
+        else:
+            self.head = new_element
 
-e1 = Element(1)
-e2 = Element(2)
-e3 = Element(3)
-e4 = Element(4)
+    def insert_first(self, new_element):
+        "Insert new element as the head of the LinkedList"
+        new_element.next = self.head
+        self.head = new_element
+        
 
-# Start setting up a Stack
-stack = stack(e1)
+    def delete_first(self):
+        "Delete the first (head) element in the LinkedList as return it"
+        if self.head:
+            deleted_element = self.head
+            temp = deleted_element.next
+            self.head = temp
+            return deleted_element
+        else:
+            return None
 
-# Test stack functionality
-stack.push(e2)
-stack.push(e3)
+class stack(object):
+    def __init__(self,top=None):
+        self.ll = LinkedList(top)
 
-@pytest.mark.parametrize("result",[(3),(2),(1)])
-def test_pop1(result):    
-    assert stack.pop().value == result
+    def push(self, new_element):
+        "Push (add) a new element onto the top of the stack"
+        self.ll.insert_first(new_element)
+        
 
-@pytest.mark.parametrize("result",[(None)])
-def test_pop2(result):    
-    assert stack.pop() == result
-
-@pytest.mark.parametrize("result",[(4)])
-def test_pop3(result):
-    stack.push(e4)    
-    assert stack.pop().value == result
-
+    def pop(self):
+        "Pop (remove) the first element off the top of the stack and return it"
+        self.ll.delete_first()
