4110380fe3e30d0204880bddec247e8f74a5e9d8
Tejasvy25 <vardisaitejasvy@msitprogram.net>
2021-07-19 06:27:37 +0530

    day4

--- 03-quicksort-Python/quicksort.py
+++ 03-quicksort-Python/quicksort.py
@@ -2,5 +2,14 @@
 Input a list.
 Output a sorted list."""
 def quicksort(array):
-	# Your code goes here
-	pass
+    # Your code goes here
+    if (len(array) < 2):
+        return array
+    else:
+        first = array[0]  
+        rest = array[1:]
+        
+        low = [x for x in rest if x < first]
+        high = [x for x in rest if x >= first]
+    quick =quicksort(low) + [first] + quicksort(high)
+    return quick
