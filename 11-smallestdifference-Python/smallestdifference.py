a4ea966bde631bc7af552a76350ed3dbeedc87ec
Tejasvy25 <vardisaitejasvy@msitprogram.net>
2021-07-20 12:34:35 +0530

    co

--- 11-smallestdifference-Python/smallestdifference.py
+++ 11-smallestdifference-Python/smallestdifference.py
@@ -4,21 +4,36 @@
 #       assert(smallestDifference([19,2,83,6,27]) == 4)
 # The two closest numbers in that list are 2 and 6, and their difference is 4.
 
-def readArray():
-    t = input().split(" ")
-    L = []
-    for i in t:
-        if len(i) != 0:
-            L.append(int(i))
-    return L
+# def readArray():
+#     t = input().split(" ")
+#     L = []
+#     for i in t:
+#         if len(i) != 0:
+#             L.append(int(i))
+#     return L
+
+# def smallestdifference(a):
+#     if(a==[]):
+#         return(-1)
+#     c=10**20
+#     for i in range(len(a)-1):
+#         for j in range(i+1,len(a)):
+#             if(abs(a[i]-a[j])<c):
+#                 c=abs(a[i]-a[j])
+#     return (c)
 
 def smallestdifference(a):
-    if(a==[]):
-        return(-1)
-    c=10**20
-    for i in range(len(a)-1):
+    if(len(a)==0):
+        return -1
+    a.sort()
+    l=[]
+    dif=0
+    
+    for i in range(len(a)):
+        
         for j in range(i+1,len(a)):
-            if(abs(a[i]-a[j])<c):
-                c=abs(a[i]-a[j])
-    return (c)
+            dif=abs(a[i]-a[j])
+            l.append(dif)        
+    # your code goes here
+    return min(l)
     
