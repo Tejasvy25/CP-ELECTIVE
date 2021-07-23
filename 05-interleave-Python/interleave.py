1ac7a40d062f71f9e4deaf8dc5ff60830d9b8064
Tejasvy25 <vardisaitejasvy@msitprogram.net>
2021-07-23 13:32:28 +0530

    day8

--- 05-interleave-Python/interleave.py
+++ 05-interleave-Python/interleave.py
@@ -8,5 +8,14 @@
 
 
 def fun_interleave(s1,s2):
-	return ""
+    s=""
+    if(len(s1)>len(s2)):
+        for i in range(0,len(s2)):
+            s+=s1[i]+s2[i]
+        s+=s1[len(s2):]
+    else:
+	    for i in range(0,len(s1)):
+		    s+=s1[i]+s2[i]
+	    s+=s2[len(s1):]
+    return s
 	
