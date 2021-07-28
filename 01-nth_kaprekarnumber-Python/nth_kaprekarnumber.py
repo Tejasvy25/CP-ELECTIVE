a8e1274876715b4c36aedb5579305e2dd0445521
Tejasvy25 <vardisaitejasvy@msitprogram.net>
2021-07-28 15:30:59 +0530

    a

--- 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
+++ 01-nth_kaprekarnumber-Python/nth_kaprekarnumber.py
@@ -7,21 +7,53 @@
 # and returns the nth Kaprekar number, where as usual we start counting at n==0.
 
 
-def kaprekar(n):
-    s=str(n*n)
-    for i in range(1,len(s)):s
-    t=int(s[:i]) + int(s[i:])
-    if t==n:
+import math
+def iskaprekar( n):
+    if n == 1 :
         return True
-    exit()
+     
+    #Count number of digits in square
+    sq_n = n * n
+    count_digits = 1
+    while not sq_n == 0 :
+        count_digits = count_digits + 1
+        sq_n = sq_n / 10
+     
+    sq_n = n*n  # Recompute square as it was changed
+     
+    # Split the square at different poitns and see if sum
+    # of any pair of splitted numbers is equal to n.
+    r_digits = 0
+    while r_digits< count_digits :
+        r_digits = r_digits + 1
+        eq_parts = (int) (math.pow(10, r_digits))
+         
+        # To avoid numbers like 10, 100, 1000 (These are not
+        # Karprekar numbers
+        if eq_parts == n :
+            continue
+         
+        # Find sum of current parts and compare with n
+         
+        sum = sq_n/eq_parts + sq_n % eq_parts
+        if sum == n :
+            return True
+     
+    # compare with original number
     return False
+     
 def fun_nth_kaprekarnumber(n):
-    f = 0
-    g = 0
-    while(f<=abs(n)):
-        g+=1
-        if(kaprekar(g)):
-            f+=1
-        return g
-print(fun_nth_kaprekarnumber(5))
+    i=1
+    j=0
+    while i<10000 and j<n:
+        if (iskaprekar(i)) :
+            j=j+1
+        i=i+1
+        print(i)
+    return i
+   
+     
+  
+    
+
     
