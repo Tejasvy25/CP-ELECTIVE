d10f2f424f420459d6c2b25bfe1b19d1df7b74e2
Tejasvy25 <vardisaitejasvy@msitprogram.net>
2021-08-02 14:46:52 +0530

    day14

--- 01-isrectangular-Python/isrectangular.py
+++ 01-isrectangular-Python/isrectangular.py
@@ -4,8 +4,12 @@
 
 
 def fun_isrectangular(l):
-	# Your code goes here...
-	return False
+    for i in range(len(l)-1):
+       
+        if(len(l[i])!=len(l[i+1])):
+            return False
+    return True
+#print(fun_isrectangular([[1, 2, 3], [1,1, 1]]))
 
 
 
